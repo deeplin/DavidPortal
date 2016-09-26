932fc331e2310bdeaec3ea82413153c6520ca779
deeplin <10525677@qq.com>
2016-09-26 17:34:16 +0800

    First commit

---
+++ DavidCore/Entities/Device.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DavidCore.Entities
+{
+    public class Device
+    {
+        private string deviceId;
+        private string model;
+        private DateTime productionDate;
+        private string natIP;
+        private int natPort;
+        private double latitude;
+        private double longitude;
+        private float radius;
+        private string locationAddress;
+        private string locationDescribe;
+        public string DeviceId
+        {
+            get
+            {
+                return deviceId;
+            }
+
+            set
+            {
+                deviceId = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+
+            set
+            {
+                model = value;
+            }
+        }
+        public DateTime ProductionDate
+        {
+            get
+            {
+                return productionDate;
+            }
+
+            set
+            {
+                productionDate = value;
+            }
+        }
+        public string NatIP
+        {
+            get
+            {
+                return natIP;
+            }
+
+            set
+            {
+                natIP = value;
+            }
+        }
+        public int NatPort
+        {
+            get
+            {
+                return natPort;
+            }
+
+            set
+            {
+                natPort = value;
+            }
+        }
+        public double Latitude
+        {
+            get
+            {
+                return latitude;
+            }
+
+            set
+            {
+                latitude = value;
+            }
+        }
+        public double Longitude
+        {
+            get
+            {
+                return longitude;
+            }
+
+            set
+            {
+                longitude = value;
+            }
+        }
+        public float Radius
+        {
+            get
+            {
+                return radius;
+            }
+
+            set
+            {
+                radius = value;
+            }
+        }
+        public string LocationAddress
+        {
+            get
+            {
+                return locationAddress;
+            }
+
+            set
+            {
+                locationAddress = value;
+            }
+        }
+        public string LocationDescribe
+        {
+            get
+            {
+                return locationDescribe;
+            }
+
+            set
+            {
+                locationDescribe = value;
+            }
+        }
+    }
+}
---
+++ DavidPortal/Controllers/DeviceController.cs
@@ -0,0 +1,24 @@
+﻿using DavidCore.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DavidPortal.Controllers
+{
+    public class DeviceController : Controller
+    {
+        IDeviceRepository deviceRepository;
+        public DeviceController(IDeviceRepository deviceRepository)
+        {
+            this.deviceRepository = deviceRepository;
+        }
+
+        // GET: Device
+        public ActionResult Index()
+        {
+            return View(deviceRepository.Devices);
+        }
+    }
+}
